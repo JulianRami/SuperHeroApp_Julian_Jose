7161e0e895cbd38440f46fa489b4281a17e3da1c
JulianRami <julian.ramirez04@uptc.edu.co>
2024-09-25 17:42:43 -0500

    feat: finish view superhero

--- app/src/main/java/com/example/superheroapp/data/ui/screens/superheros/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/data/ui/screens/superheros/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.example.superheroapp.data.ui.screens.superheros
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.futuramaapi.ui.screens.characters.rv.RVSuperheroAdapter
-import com.example.superheroapp.data.ui.screens.SuperherosActivity
-import com.example.superheroapp.data.ui.screens.SuperherosActivity.Companion.SUPERHERO_ID
+import com.example.superheroapp.data.ui.screens.superheros.rv.RVSuperheroAdapter
 import com.example.superheroapp.data.viewmodel.SuperherosViewModel
 import com.example.superheroapp.databinding.ActivitySuperherosBinding
 import kotlinx.coroutines.launch
@@ -32,8 +28,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun initRV() {
         rvSuperheroAdapter = RVSuperheroAdapter(
-            onViewInfoClickListener = { characterId ->
-                launchCharacterActivity(characterId)
+            onViewInfoClickListener = { superheroId ->
+                launchSuperheroActivity(superheroId)
             }
         )
         binding.rvSupeheros.apply {
@@ -42,19 +38,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun launchCharacterActivity(characterId: Int) {
-        startActivity(
-            Intent(
-                this,
-                SuperherosActivity::class.java
-            ).apply {
-                putExtras(
-                    bundleOf(
-                        SUPERHERO_ID to characterId
-                    )
-                )
-            }
-        )
+    private fun launchSuperheroActivity(superheroId: Int) {
+//        startActivity(
+//            Intent(
+//                this,
+//                SuperherosActivity::class.java
+//            ).apply {
+//                putExtras(
+//                    bundleOf(
+//                        SUPERHERO_ID to superheroId
+//                    )
+//                )
+//            }
+//        )
     }
 
     @SuppressLint("NotifyDataSetChanged")
--- app/src/main/java/com/example/superheroapp/data/ui/screens/superheros/rv/RVSuperheroAdapter.kt
+++ app/src/main/java/com/example/superheroapp/data/ui/screens/superheros/rv/RVSuperheroAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.futuramaapi.ui.screens.characters.rv
+package com.example.superheroapp.data.ui.screens.superheros.rv
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
